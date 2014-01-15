fc32079b9431d6e69bd66514b33890425f4ea06e
Paul Rentschler <par117@psu.edu>
2014-01-15 16:59:27 -0500

    updated the view to output the necessary data and the template is updated to display the report appropriately

--- pwrentch/FileReferences/browser/file_references.py
+++ pwrentch/FileReferences/browser/file_references.py
@@ -1,5 +1,6 @@
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 
 class ReferenceListView(BrowserView):
 
@@ -7,6 +8,32 @@ class ReferenceListView(BrowserView):
 
     def __call__(self):
         """"""
+        portal_catalog = getToolByName(self.context, 'portal_catalog')
+        current_path = '/'.join(self.context.getPhysicalPath())
+        brains = portal_catalog(
+            path={'query': current_path},
+            portal_type='File',
+            show_inactive=True
+            )
+        files = { }
+        for brain in brains:
+            obj = brain.getObject()
+            extension = obj.absolute_url().split(".")[-1].upper()
+            if extension == obj.absolute_url().upper():
+                extension = "unknown"
+
+            if not files.has_key(extension):
+                files[extension] = [ ]
+            files[extension].append(obj)
+
+
+        self.files = files
+        self.file_types = sorted(files.keys())
         self.hello_name = getattr(self.context, 'hello_name', 'World')
         return self.template()
 
+    def getReferences(self, object):
+        """"""
+        reference_catalog = getToolByName(self.context, 'reference_catalog')
+        references = reference_catalog.getBackReferences(object)
+        return [ ref.getSourceObject() for ref in references ]
