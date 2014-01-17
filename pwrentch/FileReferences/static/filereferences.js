05894ac9b71288bf7b8b271057b1c063627d8aa1
Paul Rentschler <par117@psu.edu>
2014-01-17 10:37:53 -0500

    document types now has select/unselect all functionality

--- pwrentch/FileReferences/static/filereferences.js
+++ pwrentch/FileReferences/static/filereferences.js
@@ -11,5 +11,34 @@ jQuery(function($) {
 
         // define the options dialog overlay
         $(".documentFirstHeading > a").overlay();
+
+
+        // provide select all functionality for the document types
+        $("h3.docTypes").append(
+            '<a href="" class="selectAll" rel="all">unselect all</a>'
+        );
+        $("h3.docTypes a.selectAll").click( function(e) {
+            e.preventDefault();
+            if ($(this).text() == "unselect all") {
+                // unselect all
+                $("ul.docTypes input").removeAttr("checked");
+                $(this).text("select all");
+
+            } else {
+                // select all
+                $("ul.docTypes input").attr("checked", "checked");
+                $(this).text("unselect all");
+            }
+        });
+
+        // determine the initial state of the select all link
+        $("ul.docTypes input").each( function() {
+            if ($(this).attr("checked") != "checked" &&
+                $(this).attr("checked") != true
+            ) {
+                $("h3.docTypes a.selectAll").text("select all");
+                return;
+            }
+        });
     });
 });
