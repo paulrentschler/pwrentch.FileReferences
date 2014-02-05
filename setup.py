6a8e52458f56abe67240bd1fe64d634bc6c66b34
Paul Rentschler <paul@rentschler.ws>
2014-02-05 19:41:27 +0000

    updated the setup files in preparation for packaging and uploading to PyPI and plone.org

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 This module contains the tool of pwrentch.FileReferences
 """
 import os
-from setuptools import setup, find_packages
+from distutils.core import setup, find_packages
 
 
 def read(*rnames):
@@ -25,18 +25,19 @@ tests_require = ['zope.testing']
 setup(name='pwrentch.FileReferences',
       version=version,
       description="Lists file and image items and the items that link to them",
-      long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        'Framework :: Plone',
-        'Intended Audience :: Developers',
+        'Programming Language :: Python',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
         ],
-      keywords='',
+      keywords='Plone FileReferences',
       author='Paul Rentschler',
       author_email='paul@rentschler.ws',
       url='https://github.com/paulrentschler/pwrentch.FileReferences',
+      download_url='https://github.com/paulrentschler/pwrentch.FileReferences/tarball/1.0'
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['pwrentch', ],
